e0f0bd37aff1352973f4c39f293481d555469b11
steve <saint@aliyun.com>
2016-12-29 14:48:20 +0800

    add doc

---
+++ flakor/include/runtime/lang/Ref2.h
@@ -0,0 +1,18 @@
+//zero count table
+//
+class RefZCT
+{
+
+}
+
+void deleteRef(Node node) {
+	decrementRC(node);
+	if(node.refCount == 0)
+			add node to ZCT
+}
+
+void update
+
+void reconcile() {
+	
+}
